a3850c3ed7654a19b6f739fc4ac1e1c1cfb0b504
chonkyloutre <chonkyloutre@users.noreply.github.com>
2021-01-15 12:15:59 +0100

    CHONKY

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let chonky;
+let meteorite;
+let background;
+let coeur;
+
+
+function preload() {
+  background = loadImage('img/background.png');
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(20);
+}
