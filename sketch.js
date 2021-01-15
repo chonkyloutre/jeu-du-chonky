e1e60c551a67f9483f3ba4a6ae0b5501c26e679c
chonkyloutre <chonkyloutre@users.noreply.github.com>
2021-01-15 12:53:46 +0100

    chonky + coeur1

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+// initialisation des variables
 let chonky;
 let meteorite;
 let background;
@@ -5,14 +6,25 @@ let coeur;
 
 
 function preload() {
+  // chargements des images
   background = loadImage('img/background.png');
+  chonky = loadImage('img/chonky.png');
+  coeur = loadImage('img/coeur.png');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  image(background, 0, 0);
+
+  // resize
+  background.resize(700, 700);
+  chonky.resize(120, 100);
+  coeur.resize(50, 45);
+
+  //position images 
+  image(background, 400, 10);
+  image(chonky, 700, 550);
+  image(coeur, 900, 20);
+
 }
 
-function draw() {
-  background(200);
-}
+
