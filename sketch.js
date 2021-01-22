954f97c32a83d421f8ac8373bdc63c758d3a617b
chonkyloutre <chonkyloutre@users.noreply.github.com>
2021-01-22 11:57:00 +0100

    update 22/01

--- sketch.js
+++ sketch.js
@@ -3,11 +3,18 @@ let chonky;
 let meteorite;
 let background;
 let coeur;
+let posX = 650;
+let posY = 600;
+let vX = 0;
+let vY = 0;
+let limDroite = 1410;
+let limGauche = 10;
+
 
 
 function preload() {
   // chargements des images
-  background = loadImage('img/background.png');
+  background = loadImage('img/background.jpg');
   chonky = loadImage('img/chonky.png');
   coeur = loadImage('img/coeur.png');
 }
@@ -16,19 +23,53 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // resize
-  background.resize(700, 700);
+  background.resize(1580, 780);
   chonky.resize(120, 100);
   coeur.resize(50, 45);
 
-  //position images 
-  image(background, 400, 10);
-  image(chonky, 700, 550);
+}
 
-  //3 = nombre de vie
-  for (i = 0, i < 3, i++) {
-    image(coeur,);
+function draw() {
+
+  posX += vX;
+  posY += vY;
+
+  if (posX >= limDroite) {
+    vX = 0;
   }
+  else if (posX <= limGauche) {
+    vX = 0;
+  }
+
+  if (posY < 600) {
+    vY += 0.1;
+  }
+
+  //position images 
+  image(background, -2, 0);
+  image(chonky, posX, posY);
 
 }
 
 
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    vX = -10;
+  }
+  if (keyCode === RIGHT_ARROW) {
+    vX = 10;
+  }
+  if (keyCode === 32) { //32 = espace
+    vY = -5;
+  }
+}
+
+function keyReleased() {
+  if (vX === -10) {
+    vX = 0;
+  } else {
+    vX = 0;
+  }
+  return false;
+}
